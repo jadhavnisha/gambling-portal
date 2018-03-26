a09e01e7ca44a433fe7bfcd3553212d0391ec493
Prajyot Rajigare <prajyot.rajigare27@gmail.com>
2018-03-26 18:50:49 +0530

    falsh message added, ui for contest listing has been added

--- routes/users.js
+++ routes/users.js
@@ -15,14 +15,21 @@ router.post('/signup', function(req, res, next) {
   return models.user
     .create(userData)
     //.then(user => res.status(201).send(user))
-    .then(user => res.render('signin'))
-    .catch(error => res.status(400).send(error));
+    .then(user => {
+      req.flash('info', 'Signed up successfuly, use your credetials to signin.');
+      res.render('signin');})
+    .catch(error => {
+      req.flash('info', error.errors[0].message);
+      res.render('signup');
+      // res.status(400).send(error)
+    });
 });
 
 /* render signup form */
 router.get('/signup', function(req, res, next) {
   if(req.session.userId)
   {
+    req.flash('info', 'You are already signed in, please logout and sign up.');
     res.redirect('dashboard');
   }else{
     res.render('signup');
@@ -33,6 +40,7 @@ router.get('/signup', function(req, res, next) {
 router.get('/signin', function(req, res, next) {
   if(req.session.userId)
   {
+    req.flash('info', 'You are already signed in.');
     res.redirect('dashboard');
   }else{
     res.render('signin');
@@ -59,13 +67,17 @@ router.post('/signin', function(req, res, next) {
   if (req.body.email && req.body.password) {
     return models.user.authenticate(req.body.email, req.body.password)
     .then(user => {
+      req.flash('info', 'Signed in successfuly.!!!');
       req.session.userId = user.id;
       req.session.isAdmin = user.isAdmin;
+      req.session.userBalance = user.balance;
+      req.session.userName = user.firstName + ' ' + user.lastName;
       res.redirect('dashboard');
     })
     .catch(error => {
       console.log(error);
-      res.status(404).send('Wrong email/password');
+      req.flash('info', 'Wrong credetials, please re-enter correct email & password.');      
+      res.render('signin');
     });
   }
 })
