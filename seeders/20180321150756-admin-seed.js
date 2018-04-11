a931f0df2a19c38eead2223c7583980897622fef
nisha <jadhavnisha25@gmail.com>
2018-04-11 16:25:53 +0530

    Set publickey for admin

--- seeders/20180321150756-admin-seed.js
+++ seeders/20180321150756-admin-seed.js
@@ -7,6 +7,7 @@ module.exports = {
       lastName : 'Doe',
       email : 'johnDoe@test.com',
       password: '$2a$10$jGZYNcZSOFsKP..pWe9Xtu3LaIK2rEXVydGkiSMmMZ9edMD8bKnFm',
+      publicKey: '0x4039184b25d6aa5c9195debb0c0694a58fbba25f',
       createdAt : new Date(),
       updatedAt : new Date(),
       isAdmin: true
